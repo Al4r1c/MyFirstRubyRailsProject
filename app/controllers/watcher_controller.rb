9c39d51ccfdfd3a50e0b6fd9a6754496648de360
Al4r1c <yann.kurdijaka@wanadoo.fr>
2014-05-26 15:20:59 -0400

    Search function

--- app/controllers/watcher_controller.rb
+++ app/controllers/watcher_controller.rb
@@ -34,10 +34,91 @@ class WatcherController < ApplicationController
   end
 
   def recherche
-    file = open('http://google.fr/')
-    contents = file.read
-    puts contents
+    currencyArray = {}
 
-    render :text => '1'
+    Currency.all.each { |uneDevise|
+      currencyArray[uneDevise.curr_idSteam] = uneDevise.curr_tauxEuro
+    }
+
+
+    listItemsFound = []
+    listFailConversionItem = []
+    listFailReachUrl = []
+
+    params['itemsId'].each {
+        |unIdItem|
+
+      begin
+
+        watcherConcerne = Watcher.find(unIdItem)
+
+        file = open(watcherConcerne.wat_lien)
+        contents = file.read
+
+        hash = JSON.parse contents
+
+
+        listItemsForOneId = []
+        listFailConversionForItem = []
+
+        @prixSave = nil
+
+        hash['listinginfo'].each {
+            |index, unItem|
+
+          if currencyArray.key? unItem['currencyid'].to_i
+            prixItem = (((unItem['price'] + unItem['fee']).to_f / 100) / currencyArray[unItem['currencyid'].to_i])
+            .round(2)
+
+            if prixItem != 0
+              if @prixSave != nil
+                prixRentable = ((prixItem * 100 / 117) - (prixItem * 4 / 100)).round(2)
+
+                if @prixSave < prixRentable
+                  #on achete car rentable
+                  listItemsForOneId << @prixSave
+                else
+                  break
+                end
+              end
+
+              @prixSave = prixItem
+            end
+
+          else
+            listFailConversionForItem << ['item' => watcherConcerne.wat_libelle, 'currencyid' => unItem['currencyid']]
+          end
+        }
+
+        if listItemsForOneId.any?
+          tempList = watcherConcerne.attributes
+          tempList['itemTrouves'] = listItemsForOneId
+          listItemsFound.push(tempList)
+        end
+
+        if listFailConversionForItem.any?
+          listFailConversionItem.push(watcherConcerne.id => listFailConversionForItem)
+        end
+
+      rescue
+        listFailReachUrl.push(watcherConcerne.id => watcherConcerne.wat_libelle)
+      end
+    }
+
+    arrayResultToSend = {:success => true}
+
+    if listItemsFound.any?
+      arrayResultToSend['listItems'] = listItemsFound
+    end
+
+    if listFailConversionItem.any?
+      arrayResultToSend['failConversion'] = listFailConversionItem
+    end
+
+    if listFailReachUrl.any?
+      arrayResultToSend['failReach'] = listFailReachUrl
+    end
+
+    render :json => arrayResultToSend
   end
 end
